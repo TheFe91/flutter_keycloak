1434e45eec2c7e684e043ca95ece026284a72187
Alessandro Defendenti <alessandrodefendenti@gmail.com>
2021-03-11 17:18:37 +0100

    updated plugin and gitignore

--- lib/flutter_keycloak.dart
+++ lib/flutter_keycloak.dart
@@ -1,6 +1,5 @@
 library flutter_keycloak;
 
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
@@ -8,21 +7,23 @@ import 'package:dio/dio.dart';
 import 'token_storage.dart';
 import 'utils.dart';
 
-/// Basic Headers for requests
-const basicHeaders = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/x-www-form-urlencoded',
-};
-
 /// A Flutter plugin to manage the keycloak authentication,
 /// managing on the local storage the tokens and the credentials
 class FlutterKeycloak {
+
+  /// Get KeyCloak Realm Configuration
+  Future getConf(String url) async {
+    final response = await Dio().get(url);
+    return response.data;
+  }
+
   /// Real login process
-  Future performLogin(dynamic conf,
-      String username,
-      String password, {
-        String scope = 'info',
-      }) async {
+  Future performLogin(
+    dynamic conf,
+    String username,
+    String password, {
+    String scope = 'info',
+  }) async {
     final resource = conf['resource'];
     final realm = conf['realm'];
     final credentials = conf['credentials'];
@@ -30,90 +31,49 @@ class FlutterKeycloak {
     final url =
         '${getRealmURL(realm, authServerUrl)}/protocol/openid-connect/token';
 
-    final response = await Dio().post(
-      url,
-      data: {
-        'grant_type': 'password',
-        'username': username,
-        'password': password,
-        'client_id': Uri.encodeComponent(resource),
-        'client_secret': credentials ? credentials.secret : null,
-        'scope': scope,
-      },
-      options: Options(
-        headers: basicHeaders,
-      ),
-    );
+    try {
+      final dio = Dio();
+      dio.options.headers[HttpHeaders.acceptHeader] = 'application/json';
+      dio.options.headers[HttpHeaders.contentTypeHeader] =
+          'application/x-www-form-urlencoded';
+      final response = await dio.post(
+        url,
+        data: {
+          'grant_type': 'password',
+          'username': username,
+          'password': password,
+          'client_id': Uri.encodeComponent(resource),
+          'client_secret': credentials != null ? credentials['secret'] : null,
+          'scope': scope,
+        },
+      );
 
-    if (response.statusCode == 200) {
-      final jsonResponse = response.data;
-      await saveConfiguration(conf);
-      await saveTokens(jsonResponse);
-      await saveCredentials({'username': username, 'password': password});
-      return jsonResponse;
+      if (response.statusCode == 200) {
+        final jsonResponse = response.data;
+        await saveConfiguration(conf);
+        await saveTokens(jsonResponse);
+        await saveCredentials({'username': username, 'password': password});
+        return jsonResponse;
+      }
+
+      print(
+        'Error during kc-api-login, '
+        '${response.statusCode}: '
+        '${response.data.toString()}',
+      );
+    } catch (e) {
+      print(e);
     }
-
-    print(
-      'Error during kc-api-login, '
-          '${response.statusCode}: '
-          '${response.data.toString()}',
-    );
     // throw Exce({...jsonResponse, status: fullResponse.status});
   }
 
-  /// Get the tokens from AS
-  Future retrieveTokens(dynamic conf,
-      String code,
-      Function resolve,
-      Function reject,
-      String deepLinkUrl,) async {
-    final resource = conf['resource'];
-    final realm = conf['realm'];
-    final redirectUri = conf['redirectUri'];
-    final credentials = conf['credentials'];
-    final authServerUrl = conf['auth-server-url'];
-
-    final tokenUrl =
-        '${getRealmURL(realm, authServerUrl)}/protocol/openid-connect/token';
-
-    final headers = basicHeaders;
-
-    if (credentials != null && credentials['secret'] != null) {
-      final stringToBase64 = utf8.fuse(base64);
-      final encodedKey =
-      stringToBase64.encode('$resource:${credentials['secret']}');
-      headers[HttpHeaders.authorizationHeader] = 'Basic $encodedKey';
-    }
-
-    final response = await Dio().post(
-      tokenUrl,
-      data: {
-        'grant_type': 'authorization_code',
-        'redirect_uri': redirectUri,
-        'client_id': resource,
-        'code': code,
-      },
-      options: Options(headers: headers),
-    );
-
-    final jsonResponse = response.data;
-
-    if (response.statusCode == 200) {
-      await saveConfiguration(conf);
-      await saveTokens(jsonResponse);
-      resolve({'tokens': jsonResponse, 'deepLinkUrl': deepLinkUrl});
-    } else {
-      print('Error during kc-retrieve-tokens');
-      reject(jsonResponse);
-    }
-  }
-
   /// Init the login process
-  void login(dynamic conf,
-      String username,
-      String password, {
-        String scope = 'info',
-      }) async =>
+  Future login(
+    dynamic conf,
+    String username,
+    String password, {
+    String scope = 'info',
+  }) async =>
       await performLogin(conf, username, password, scope: scope);
 
   /// Automatically redo the login
@@ -159,16 +119,14 @@ class FlutterKeycloak {
     final userInfoUrl =
         '${getRealmURL(realm, authServerUrl)}/protocol/openid-connect/userinfo';
 
-    final headers = basicHeaders;
-    headers[HttpHeaders.authorizationHeader] =
-    'Bearer ${savedTokens['access_token']}';
+    final dio = Dio();
+    dio.options.headers[HttpHeaders.acceptHeader] = 'application/json';
+    dio.options.headers[HttpHeaders.contentTypeHeader] =
+        'application/x-www-form-urlencoded';
+    dio.options.headers[HttpHeaders.authorizationHeader] =
+        'Bearer ${savedTokens['access_token']}';
 
-    final response = await Dio().get(
-      userInfoUrl,
-      options: Options(
-        headers: headers,
-      ),
-    );
+    final response = await dio.get(userInfoUrl);
 
     if (response.statusCode == 200) {
       return response.data;
@@ -201,7 +159,12 @@ class FlutterKeycloak {
     final refreshTokenUrl =
         '${getRealmURL(realm, authServerUrl)}/protocol/openid-connect/token';
 
-    final response = await Dio().post(
+    final dio = Dio();
+    dio.options.headers[HttpHeaders.acceptHeader] = 'application/json';
+    dio.options.headers[HttpHeaders.contentTypeHeader] =
+        'application/x-www-form-urlencoded';
+
+    final response = await dio.post(
       refreshTokenUrl,
       data: {
         'grant_type': 'refresh_token',
@@ -209,7 +172,6 @@ class FlutterKeycloak {
         'client_id': Uri.encodeComponent(resource),
         'client_secret': credentials != null ? credentials['secret'] : null,
       },
-      options: Options(headers: basicHeaders),
     );
     final jsonResponse = await response.data;
 
@@ -239,15 +201,19 @@ class FlutterKeycloak {
       throw 'Error during kc-logout, savedTokens is $savedTokens';
     }
 
-    final logoutUrl = '${getRealmURL(
-        realm, authServerUrl)}/protocol/openid-connect/logout';
-    final response = await Dio().get(
-      logoutUrl,
-      options: Options(headers: basicHeaders),
-    );
+    final logoutUrl =
+        '${getRealmURL(realm, authServerUrl)}/protocol/openid-connect/logout';
+
+    final dio = Dio();
+    dio.options.headers[HttpHeaders.acceptHeader] = 'application/json';
+    dio.options.headers[HttpHeaders.contentTypeHeader] =
+        'application/x-www-form-urlencoded';
+
+    final response = await dio.get(logoutUrl);
 
     if (response.statusCode == 200) {
       await clearSession();
+      return;
     }
 
     throw 'Error during kc-logout: ${response.statusCode}: ${response.data}';
